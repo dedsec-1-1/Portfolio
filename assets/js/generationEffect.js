275c28ed3e6f970dff4cb0ac6cb94789f8e6dad1
dedsec-1-1 <ivo.ndov11@gmail.com>
2024-12-21 21:32:54 +0200

    Initial commit: HTML Structure, Generation & GLITCH effects

---
+++ assets/js/generationEffect.js
@@ -0,0 +1,22 @@
+const SYMBOLS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[{]}\\|;:,<.>/?"
+
+document.getElementById("navigation-title").onmouseover = event => {
+    let iterations = 0;
+    const textLength = event.target.dataset.value.length;
+
+    const interval = setInterval(() => {
+        event.target.innerText = event.target.innerText.split("")
+            .map((symbol, index) => {
+                if (index < iterations) {
+                    return event.target.dataset.value[index];
+                }
+
+                return SYMBOLS[Math.floor(Math.random() * SYMBOLS.length)];
+            })
+            .join("");
+
+        if (iterations >= textLength) clearInterval(interval);
+
+        iterations += 1;
+    }, 70);
+};
